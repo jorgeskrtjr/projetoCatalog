73bab3f732053e04643d855964975da4f66f8817
Jorge Silva Júnior <silva.junior@pmu.net>
2022-12-05 14:23:34 -0300

    database seeding

---
+++ backend/src/main/resources/import.sql
@@ -0,0 +1,5 @@
+INSERT INTO tb_category(name) VALUES('Books');
+INSERT INTO tb_category(name) VALUES('Eletronics');
+INSERT INTO tb_category(name) VALUES('Moovies');
+INSERT INTO tb_category(name) VALUES('House');
+INSERT INTO tb_category(name) VALUES('Computers');
